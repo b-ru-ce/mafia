91715df6bff13b4b6ceba68712f1074dfed19689
JewelSam <mrswlasowa@gmail.com>
2014-05-28 09:09:39 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mafia::Application.config.secret_key_base = 'f850751a9b7d4adb682ef44585815e53460a3ce995ad8a30fac8eb7b174c7a87110e65ee39d82d4e77ee961ceb7cc635800bb227308bed05dd90d5f06dfd792b'
